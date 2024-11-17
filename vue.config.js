75aafc8c8266461e7fce9e3e459a66557b5f2769
fernando <fleitasbaez55@gmail.com>
2024-11-17 09:34:06 -0300

    Correcion de archivos de nuevo

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,7 @@
-const { defineConfig } = require('@vue/cli-service');
-const CompressionPlugin = require('compression-webpack-plugin');
-
+const {defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies : true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/gestor_tareas_vue/'
-    : '/',
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        algorithm: 'gzip', // Algoritmo para comprimir (gzip es estándar)
-        test: /\.(js|css|html|svg)$/, // Tipos de archivos que serán comprimidos
-        threshold: 10240, // Tamaño mínimo en bytes para comprimir (10 KiB)
-        minRatio: 0.8, // Relación mínima de compresión (80%)
-      }),
-    ],
-  },
-});
+   ? '/Gestor-de-Vue-por-fin/'
+   : '/'
+})

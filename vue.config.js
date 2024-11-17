126a91e4108a45d1ae4e7eeb31024be8ad311e17
fernando <fleitasbaez55@gmail.com>
2024-11-16 23:13:18 -0300

    Compresion de archivos en vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/gestor_tareas_vue/'
-    : '/'
-})
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin({
+        algorithm: 'gzip', // Algoritmo para comprimir (gzip es estándar)
+        test: /\.(js|css|html|svg)$/, // Tipos de archivos que serán comprimidos
+        threshold: 10240, // Tamaño mínimo en bytes para comprimir (10 KiB)
+        minRatio: 0.8, // Relación mínima de compresión (80%)
+      }),
+    ],
+  },
+});
